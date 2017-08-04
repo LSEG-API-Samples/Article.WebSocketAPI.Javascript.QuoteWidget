b2351bbdd6f62b55f20ff65d91406bb75cd98feb
zinc1oxide@gmail.com <zinc1oxide@gmail.com>
2017-08-04 08:39:45 -0400

    Updated text output.

--- quoteObject.js
+++ quoteObject.js
@@ -21,7 +21,7 @@
     
     // Configuration
     app.constant('config', {
-        wsServer: '10.67.4.99:15000',   // Address of our Eikon WebSocket server.  Format: <ip>:<port>
+        wsServer: '<host:port>',        // Address of our Eikon WebSocket server.  Eg: ads:15000
         wsLogin: {                      // Elektron WebSocket login credentials
             user: 'user',
             appId: '256',
